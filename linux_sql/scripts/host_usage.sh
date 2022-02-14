fb2463e3637f89577a4fa7e5cafeafa27fd03a37
Hai Long Do <long2qw@gmail.com>
2022-02-14 18:42:30 +0000

    fixed typed error

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -22,20 +22,20 @@ mem_out=`cat /proc/meminfo`
 
 #usage specs
 mem_free=$(echo "$mem_out" | egrep "MemFree:" | awk '{print $2}' | xargs)
-cpu_idle=$(vmstat -t | tail -4 | awk '{print $15}' | xargs)
-cpu_kernel=$(vmstat -t | tail -6 | awk '{print $13}' | xargs)
+cpu_idle=$(vmstat -t | tail -4 | awk '{print $15}' | sed 's/id//' | xargs)
+cpu_kernel=$(vmstat -t | tail -6 | awk '{print $13}' | sed 's/us//' | xargs)
 disk_io=$(vmstat -d | tail -1 | awk '{print $10}' | xargs)
-disk_available=$(echo "$df_out" | tail -3 | awk '{print $3}' | xargs)
-total_mem=$(echo "$mem_out" | egrep "MemTotal:" | awk '{print $2}' | xargs)
+disk_available=$(echo "$df_out" | tail -3 | awk '{print $4}' | sed 's/Available//' | sed 's/M//' | xargs)
 timestamp=$(vmstat -t | tail -1 | awk '{print $18}' | xargs)
 
 
 #insert query to host_usage table
-insert_query="INSERT INTO host_usage (hostname, mem_free, cpu_idle, cpu_kernel, disk_io, disk_available, total_mem, timestamp) VALUES ('$hostname', '$mem_free', '$cpu_idle', '$cpu_kernel', '$disk_io', '$disk_available', '$total_mem', '$timestamp')"
+insert_query="INSERT INTO host_usage (host_id, mem_free, cpu_idle, cpu_kernel, disk_io, disk_available, timestamp)
+VALUES ((SELECT id FROM host_info WHERE hostname = '$hostname'), '$mem_free', '$cpu_idle', '$cpu_kernel', '$disk_io', '$disk_available', '$timestamp')"
 
 export PGPASSWORD=$psql_password
 
-psql -h $psql_host -p $port -d $db_name -U $psql_user -c $insert_query
+psql -h $psql_host -p $port -d $db_name -U $psql_user -c "$insert_query"
 
 exit 0
 
