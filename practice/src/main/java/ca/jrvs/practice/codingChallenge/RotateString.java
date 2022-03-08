957fa6797e11014819696d452284f024b568b8b4
Hai Long Do <long2qw@gmail.com>
2022-03-08 16:58:01 -0500

    Added big O for rotate string

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/RotateString.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/RotateString.java
@@ -2,6 +2,10 @@ package ca.jrvs.practice.codingChallenge;
 
 public class RotateString {
 
+  /**
+   * Big O: O(n^2) Justification: contains have runtime of O(nm) but in this situation we have the
+   * same string so it will be O(n^2)
+   */
   public boolean rotateString(String s, String goal) {
     return s.length() == goal.length() && (s + s).contains(goal);
   }
