93137b7a84bf7edb307f01f71109ccfa27d3f8bb
Hai Long Do <long2qw@gmail.com>
2022-04-11 08:59:57 -0400

    Implmemented lexQuote

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/AppConfig.java
@@ -0,0 +1,46 @@
+package ca.jrvs.apps.trading;
+
+import ca.jrvs.apps.trading.model.config.MarketDataConfig;
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+//@EnableTransactionManagement
+@ComponentScan("ca.jrvs.apps.trading")
+public class AppConfig {
+
+  private Logger logger = LoggerFactory.getLogger(AppConfig.class);
+
+  @Bean
+  public MarketDataConfig marketDataConfig() {
+    MarketDataConfig marketDataConfig = new MarketDataConfig();
+    marketDataConfig.setToken(System.getenv("IEX_PUB_TOKEN"));
+    marketDataConfig.setHost("https://cloud.iexapis.com/v1");
+    return marketDataConfig;
+  }
+
+
+  @Bean
+  public HttpClientConnectionManager httpClientConnectionManager() {
+    PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
+    cm.setMaxTotal(50);
+    cm.setDefaultMaxPerRoute(50);
+    return cm;
+  }
+
+  public static void main(String[] args) throws Exception {
+    ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+    Application app = context.getBean(Application.class);
+    app.run(args);
+  }
+
+}
